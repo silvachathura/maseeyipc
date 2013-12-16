609a60b8aa0a640c59205c9349bac663ed4b45b2
chathura roshan silva <silvachathura@rocketmail.com>
2013-12-17 01:22:05 +1300

    3

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ function scroll(){
 }
 document.addEventListener('DOMContentLoaded', scroll, false);
 
-$('#tab-bar a').on('click', function(e){
-    e.preventDefault();
-    var nextPage = $(e.target.hash);
-    $("#pages .current").removeClass("current");
-    nextPage.addClass("current");
-})
+// $('#tab-bar a').on('click', function(e){
+    // e.preventDefault();
+    // var nextPage = $(e.target.hash);
+    // $("#pages .current").removeClass("current");
+    // nextPage.addClass("current");
+// })
 
-function page(toPage) {
-    var toPage = $(toPage),
-    fromPage = $("#pages .current");
-    if(toPage.hasClass("current") || toPage === fromPage) {
-        return;
-    };
-    toPage.addClass("current fade in").one("webkitAnimationEnd", function(){
-        fromPage.removeClass("current fade out");
-        toPage.removeClass("fade in")
-    });
-    fromPage.addClass("fade out");
-}
+// function page(toPage) {
+    // var toPage = $(toPage),
+    // fromPage = $("#pages .current");
+    // if(toPage.hasClass("current") || toPage === fromPage) {
+        // return;
+    // };
+    // toPage.addClass("current fade in").one("webkitAnimationEnd", function(){
+        // fromPage.removeClass("current fade out");
+        // toPage.removeClass("fade in")
+    // });
+    // fromPage.addClass("fade out");
+// }
